2b7c8d6f7b256ce18941d610ae46e055013b7bcb
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-01-15 12:00:59 +0300

    feat: change in UaClient session start and subscriptions, change ClientBootstrap StartClient loop
    UaClient:
    - Create session using TraceableSessionFatory instance
    - Reconnect handler created when session created and disposed only when
      session disconnected
    - Set default params to session:
        - Not delete subscriptions on close
        - Transfer subscriptions on reconnect
    - Disconnect method now async with optional param leaveChannelOpen
    - Keep disposing status in field _disposed
    - AddSubscription method now async
        - Set to created subscription:
          - DisableMonitoredItemCache to true
          - Use FastDataChangeNotification callback insted using
    	MonitoreItem Notification event
          - For Monitored items set queue size to 1
    
    ClientsBootstrap:
    - Move start clients logit to StartClientsAsyn method
      - Using SemaphoreSlim  for track max simultanously clients starting
        tasks
      - Try get MaxConcurrentStartingClients value from appsettings.json (if
        can't get value use 10 as default)
    - Set logger for Opc.Ua.Utils
    
    appsettings.json:
    - Set default log level to Warning
    - Create section for ClientBootstrap settings

--- src/OpcuaAggregatioinClient/OpcuaAggregationClient.Infrastructure/ClientsBootstrap.cs
+++ src/OpcuaAggregatioinClient/OpcuaAggregationClient.Infrastructure/ClientsBootstrap.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Opc.Ua;
@@ -5,23 +6,44 @@ using OpcuaAggregationClient.Infrastructure.Entities;
 
 namespace OpcuaAggregationClient.Infrastructure;
 
-public class ClientsBootstrap(
-    UaClientConfigurationStore uaClientConfigurationStore,
-    UaClientFactory uaClientFactory,
-    ILoggerFactory loggerFactory
-) : IHostedService, IDisposable
+public class ClientsBootstrap : IHostedService, IDisposable
 {
-    private readonly UaClientConfigurationStore _uaClientConfigurationStore = uaClientConfigurationStore;
-    private readonly UaClientFactory _uaClientFactory = uaClientFactory;
-    private readonly ILoggerFactory _loggerFactory = loggerFactory;
-    private readonly ILogger<ClientsBootstrap> _logger = loggerFactory.CreateLogger<ClientsBootstrap>();
+    private readonly UaClientConfigurationStore _uaClientConfigurationStore;
+    private readonly UaClientFactory _uaClientFactory;
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ILogger<ClientsBootstrap> _logger;
     private Task? _executingTask;
     private readonly CancellationTokenSource _stoppingCts = new();
+    private SemaphoreSlim _semaphore;
     private readonly List<UaClient> _uaClients = [];
+
+    public ClientsBootstrap(
+        UaClientConfigurationStore uaClientConfigurationStore,
+        UaClientFactory uaClientFactory,
+        IConfiguration configuration,
+        ILoggerFactory loggerFactory
+)
+    {
+        _uaClientConfigurationStore = uaClientConfigurationStore;
+        _uaClientFactory = uaClientFactory;
+        _loggerFactory = loggerFactory;
+        _logger = loggerFactory.CreateLogger<ClientsBootstrap>();
+        Utils.SetLogger(_loggerFactory.CreateLogger("OpcUaUtilsLogger"));
+
+        if(int.TryParse(configuration.GetSection("ClientsBootstrapSettings:MaxConcurrentStartingClients").Value, out var maxConcurrentStartingClients))
+        {
+            _semaphore = new SemaphoreSlim(maxConcurrentStartingClients, maxConcurrentStartingClients);
+        }
+        else
+        {
+            _semaphore = new SemaphoreSlim(10,10);
+        }
+    }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        Utils.SetLogger(_loggerFactory.CreateLogger("OpcUaUtilsLogger"));
+        
+
         _executingTask = ExecuteAsync(_stoppingCts.Token);
 
         if (_executingTask.IsCompleted)
@@ -34,16 +56,7 @@ public class ClientsBootstrap(
     {
         try
         {
-            foreach (var config in await _uaClientConfigurationStore.GetUaClientConfigurationsAsync(stoppingToken))
-            {
-                if(config.Enabled == false)
-                {
-                    _logger.LogInformation("Client {sessionName} is disabled", config.SessionName);
-                    continue;
-                }
-
-                await StartUaClientAsync(config, stoppingToken);
-            }
+            await StartClientsAsync(stoppingToken);
             _logger.LogInformation("All clients started");
         }
         catch (Exception ex)
@@ -54,24 +67,42 @@ public class ClientsBootstrap(
 
         while(!stoppingToken.IsCancellationRequested)
         {
-            foreach (var config in await _uaClientConfigurationStore.GetUaClientConfigurationsAsync(stoppingToken))
-            {
-                if(_uaClients.FindIndex(c => c.ClientId == config.Id) != -1)
-                {
-                    _logger.LogInformation("Client {sessionName} is already running", config.SessionName);
-                    continue;
-                }
-
-                if(config.Enabled == false)
-                {
-                    _logger.LogInformation("Client {sessionName} is disabled", config.SessionName);
-                    continue;
-                }
-
-                await StartUaClientAsync(config, stoppingToken);
-            }
-
+            await StartClientsAsync(stoppingToken);
             await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
+        }
+    }
+
+    private async Task StartClientsAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                foreach (var config in await _uaClientConfigurationStore.GetUaClientConfigurationsAsync(stoppingToken))
+                {
+                    if (_uaClients.FindIndex(c => c.ClientId == config.Id) != -1)
+                    {
+                        _logger.LogInformation("Client {sessionName} is already running", config.SessionName);
+                        continue;
+                    }
+
+                    if (config.Enabled == false)
+                    {
+                        _logger.LogInformation("Client {sessionName} is disabled", config.SessionName);
+                        continue;
+                    }
+
+                    await _semaphore.WaitAsync(stoppingToken); // Wait to enter the semaphore
+                    _ = StartUaClientAsync(config, stoppingToken).ContinueWith(t => _semaphore.Release(), stoppingToken); // Release the semaphore after execution
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error starting clients");
+            throw;
         }
     }
 
@@ -94,7 +125,7 @@ public class ClientsBootstrap(
         _uaClients.Add(client);
 
         var channels = await _uaClientConfigurationStore.GetUaClientChannelConfigurationsAsync(config.Id.Value);
-        client.AddSubscription(channels, config.SessionName);
+        await client.AddSubscription(channels, config.SessionName);
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
