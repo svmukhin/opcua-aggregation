ad136e5dbf5b4dcf764cd4a3b55e70ce70567566
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-01-25 20:35:51 +0300

    feat: update footer styles

--- src/opcua-aggregation-wwwroot/src/components/layout/footer.ts
+++ src/opcua-aggregation-wwwroot/src/components/layout/footer.ts
@@ -4,7 +4,10 @@ export const Footer = {
   view: () =>
     m(
       'footer',
-      { class: 'w-full' },
+      {
+        class:
+          'rounded-lg border shadow-lg overflow-hidden p-2 bg-white border-stone-200 shadow-stone-950/5 mx-auto w-full max-w-screen-xl',
+      },
       m(
         'p',
         { class: 'font-sans antialiased text-base text-current text-center' },
--- src/opcua-aggregation-wwwroot/wwwroot/js/app.bundle.js
+++ src/opcua-aggregation-wwwroot/wwwroot/js/app.bundle.js
@@ -2182,7 +2182,9 @@
 	};
 
 	const Footer = {
-	    view: () => m('footer', { class: 'w-full' }, m('p', { class: 'font-sans antialiased text-base text-current text-center' }, 'OPC UA Aggregation Client')),
+	    view: () => m('footer', {
+	        class: 'rounded-lg border shadow-lg overflow-hidden p-2 bg-white border-stone-200 shadow-stone-950/5 mx-auto w-full max-w-screen-xl',
+	    }, m('p', { class: 'font-sans antialiased text-base text-current text-center' }, 'OPC UA Aggregation Client')),
 	};
 
 	const header = document.getElementById('header');
