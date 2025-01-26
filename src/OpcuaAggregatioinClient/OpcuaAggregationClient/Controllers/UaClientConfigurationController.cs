a1168932e9e6f99bc84ea15d7ff928998a82c676
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-01-26 15:13:36 +0300

    feat: create get methods for configuration entities

--- src/OpcuaAggregatioinClient/OpcuaAggregationClient/Controllers/UaClientConfigurationController.cs
+++ src/OpcuaAggregatioinClient/OpcuaAggregationClient/Controllers/UaClientConfigurationController.cs
@@ -12,34 +12,30 @@ public class UaClientConfigurationController(
     private readonly UaClientConfigurationStore _store = store;
 
     [HttpGet("client")]
-    public async Task<IActionResult> GetClientConfiguration([FromQuery] int? clientId, CancellationToken ct)
+    public async Task<IActionResult> GetClientConfiguration(CancellationToken ct)
     {
-        if (clientId is not null)
-        {
-            var config = await _store.GetUaClientConfigurationByIdAsync(clientId.Value, ct);
-            return new JsonResult(config);
-        }
-
         var configs = await _store.GetUaClientConfigurationsAsync(ct);
-
         return new JsonResult(configs);
     }
 
-    [HttpGet("channel")]
-    public async Task<IActionResult> GetClientConfigurationChannel([FromQuery] (int clientId, int? channelId) query, CancellationToken ct)
+    [HttpGet("client/{id:int}")]
+    public async Task<IActionResult> GetClientConfigurationById(int id, CancellationToken ct)
     {
-        if (query.clientId <= 0 && query.channelId is null)
-        {
-            return new JsonResult(new { Error = "At least one parameter must be set (clientId or channelId)!" });
-        }
+        var config = await _store.GetUaClientConfigurationByIdAsync(id, ct);
+        return new JsonResult(config);
+    }
 
-        if (query.channelId is not null)
-        {
-            var channel = await _store.GetUaClientChannelConfigurationByIdAsync(query.channelId.Value, ct);
-            return new JsonResult(channel);
-        }
-
-        var channels = await _store.GetUaClientChannelConfigurationsAsync(query.clientId, ct);
+    [HttpGet("client/{clientId:int}/channel")]
+    public async Task<IActionResult> GetClientConfigurationChannel(int clientId, CancellationToken ct)
+    {
+        var channels = await _store.GetUaClientChannelConfigurationsAsync(clientId, ct);
         return new JsonResult(channels);
+    }
+
+    [HttpGet("client/{clientId:int}/channel/{id:int}")]
+    public async Task<IActionResult> GetClientConfigurationChannelById(int id, CancellationToken ct)
+    {
+        var channel = await _store.GetUaClientChannelConfigurationByIdAsync(id, ct);
+        return new JsonResult(channel);
     }
 }
