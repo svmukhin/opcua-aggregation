76f1699b5727d1499ac8985d15a022430d50b174
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-02-06 16:56:36 +0300

    chore: remove unused code

--- src/opcua-aggregation-wwwroot/src/components/pages/config.page.ts
+++ src/opcua-aggregation-wwwroot/src/components/pages/config.page.ts
@@ -2,8 +2,6 @@ import m from 'mithril';
 import { IConfigPageModel } from '../../models/config/config-page.model';
 import { CardComponent } from '../shared/card.component';
 import { ClientConfigTableComponent } from '../common/config/client-config-table.component';
-import { ButtonComponent } from '../shared/button.component';
-import { ButtonGroupComponent } from '../shared/button-group.component';
 
 export class ConfigPage implements m.ClassComponent {
   constructor(private configModel: IConfigPageModel) {}
@@ -14,9 +12,6 @@ export class ConfigPage implements m.ClassComponent {
 
   view() {
     return m('div', { class: 'flex flex-col' }, [
-      m(ButtonGroupComponent, [
-        m(ButtonComponent, { onclick: () => alert('clicked') }, 'Add client'),
-      ]),
       m(
         CardComponent,
         m(ClientConfigTableComponent, { configs: this.configModel.list })
